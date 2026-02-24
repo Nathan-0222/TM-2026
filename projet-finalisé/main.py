16784fba2aace3ba274390aa76823ca3408c8096
Nathan-0222 <nathanluceadavid@gmail.com>
2026-02-24 19:57:10 +0100

    Création d'une base de code(attributs+méthodes 1)

--- projet-finalisé/main.py
+++ projet-finalisé/main.py
@@ -1,16 +1,36 @@
 class Carte:
     
-    def __init__(self, ):
+    def __init__(self, nomcarte, ):
+        self.nomcarte = nomcarte
         
 
 
 class Joueur:
+
+    def __init__(self, nom, carteatt):
+        self.en_vie = True
+        self.nom = nom
+        self.carteatt = carteatt    #carteatt pour carte attribuée
     
-    def __init__(self, ):
+    def mourir(self):
+        self.en_vie = False
+        print("Le joueur", self.nom, "est mort cette nuit ...")
+    
+    def ressuciter(self):
+        self.en_vie = True
+        print("Le joueur", self.nom, "a été ressucité cette nuit !!!")
+    
+    def voter(cible):
+
+    
 
 
 
 class Partie:
     
-    def __init__ (self, ):
+    L = []      #Liste du nb. de joueurs
+    nombre-joueurs = len(L)    #nb. de joueurs afin de savoir combien de cartes distribuées
+    
+    def __init__ (self, mdp):
+        self.mdp = mdp
          

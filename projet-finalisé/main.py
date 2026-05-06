68ef2f3365f85d2b1fa3a2d2593a979426d6d2fb
Nathan-0222 <nathanluceadavid@gmail.com>
2026-05-06 10:43:43 +0200

    1er commit

--- projet-finalisé/main.py
+++ projet-finalisé/main.py
@@ -4,15 +4,9 @@ from partie import *
 
 
 partie = Partie("ciao")
-partie.run(self)
+partie.run()
 
 c1 = Sorciere()
 
 # j1 = Joueur("Nathan", c1) # ici il faudrait passer que le nom, la carte dans un deuxième temps
 
-partie.ajouter_joueur()
-print(partie.liste_joueurs)
-
-elimine = partie.liste_joueurs.pop(1)
-print(f"{elimine.nom} est éliminé !")
-print(partie.liste_joueurs)

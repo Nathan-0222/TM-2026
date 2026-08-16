6c6d1b6e12ee410d218b6cdcc919037d797996a9
Nathan-0222 <nathanluceadavid@gmail.com>
2026-08-16 13:07:07 +0200

    création des 3 zones dans le jeu, la message qui remplace les print, la joueurs dans laquelle on voit constamment la liste, et la actions dans laquelle se déroule la partie elle même (choix, boutons, ...)

--- projet-finalisé/interface.py
+++ projet-finalisé/interface.py
@@ -49,7 +49,7 @@ def page_acceuil():
             return
 
         partie_en_cours.ajouter_joueur(nom)
-        champ_nom.value = ""   #supprime la valeur mise pour mettre case vide
+        champ_nom.value = ""   #supp la valeur mise pour mettre case vide
 
         zone_liste_accueil.refresh()
         zone_bouton_commencer.refresh()
@@ -60,5 +60,124 @@ def page_acceuil():
 
 
 @ui.page('/jeu')
-def zone_jeu():
-    pass
+def page_jeu():
+
+    if partie_en_cours.phase == "inscription":
+        ui.navigate.to('/accueil')
+        return
+
+    ui.label('Jeu du Loup Garou')
+    ui.separator()
+
+
+    @ui.refreshable
+    def zone_message():
+        ui.label(partie_en_cours.message)   #zone qui remplace print(message jeu)
+
+
+    @ui.refreshable
+    def zone_joueurs():   #zone affichant constamment la liste des joueurs (plus simple)
+        if len(partie_en_cours.liste_joueurs) == 0:
+            return
+        ui.label('Voiçi la liste des joueurs :')
+        for i in range (len(partie_en_cours.liste_joueurs)):
+            if partie_en_cours.liste_joueurs[i].envie == True:
+                ui.label(str(i + 1) + '-' + partie_en_cours.liste_joueurs[i].nom)
+            else:
+                ui.label('Décédé -' + partie_en_cours.liste_joueurs[i].nom)
+
+
+    @ui.refreshable
+    def zone_actions():
+
+
+        #cupidon
+        if partie_en_cours.phase == "cupidon":
+
+            cupidon_present = False
+            for j in range(len(partie_en_cours.liste_joueurs)):
+                if j.carteatt.nom == "cupidon" and j.envie == True:
+                    cupidon_present = True
+
+            if cupidon_present == True:
+
+                ui.label('Cupidon se réveille. Choisissez les deux amoureux :')
+                champ_j1 = ui.number(label='Numéro du 1er joueur', value=1, min=1, max=len(partie_en_cours.liste_joueurs))
+                champ_j2 = ui.number(label='Numéro du 2ème joueur', value=1, min=1, max=len(partie_en_cours.liste_joueurs))
+
+                def cliquer_cupidon():
+                    index_j1 = int(champ_j1) - 1
+                    index_j2 = int(champ_j2) - 1
+                    partie_en_cours.action_cupidon(index_j1, index_j2)
+                    zone_message.refresh()
+                    zone_joueurs.refresh()
+                    zone_actions.refresh()
+
+                ui.button('Valider le couple', on_click=cliquer_cupidon)
+
+            else:
+
+                partie_en_cours.passer_phase_suivante("cupidon")
+                zone_message.refresh()
+                zone_actions.refresh()
+
+
+        #voleur
+        elif partie_en_cours.phase == "voleur":
+        
+            voleur_present = False
+            for j in partie_en_cours.liste_joueurs:
+                if j.carteatt.nom == "Voleur" and j.envie == True:
+                    voleur_present = True
+        
+            if voleur_present == True:
+                ui.label("Le Voleur se réveille. Voulez-vous dérober la carte de quelqu'un ?")
+                champ_cible = ui.number(label="Numéro du joueur à cambrioler", value=1, min=1, max=len(partie_en_cours.liste_joueurs))
+        
+                def cliquer_voleur_oui():
+                    index_cible_voleur = int(champ_cible.value) - 1
+                    partie_en_cours.action_voleur(True, index_cible_voleur)
+                    zone_message.refresh()
+                    zone_joueurs.refresh()
+                    zone_actions.refresh()
+        
+                def cliquer_voleur_non():
+                    partie_en_cours.action_voleur(False, 0)
+                    zone_message.refresh()
+                    zone_joueurs.refresh()
+                    zone_actions.refresh()
+        
+                ui.button("Oui, je vole !", on_click=cliquer_voleur_oui)
+                ui.button("Non, je reste tranquille", on_click=cliquer_voleur_non)
+        
+            else:
+                partie_en_cours.passer_phase_nuit("voleur")
+                zone_message.refresh()
+                zone_actions.refresh()
+
+
+        #voyante
+        elif partie_en_cours.phase == "voyante":
+        
+            voyante_presente = False
+            for j in partie_en_cours.liste_joueurs:
+                if j.carteatt.nom == "Voyante" and j.envie == True:
+                    voyante_presente = True
+        
+            if voyante_presente == True:
+                ui.label("La Voyante se réveille. Choisissez un joueur à observer :")
+                champ_cible = ui.number(label="Numéro du joueur à observer", value=1, min=1, max=len(partie_en_cours.liste_joueurs))
+        
+                def cliquer_voyante():
+                    index_cible = int(champ_cible.value) - 1
+                    partie_en_cours.action_voyante(index_cible)
+                    zone_message.refresh()
+                    zone_joueurs.refresh()
+                    zone_actions.refresh()
+        
+                ui.button("Regarder", on_click=cliquer_voyante)
+        
+            else:
+                partie_en_cours.passer_phase_nuit("voyante")
+                zone_message.refresh()
+                zone_actions.refresh()
